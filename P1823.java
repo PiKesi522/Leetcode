bf5bc1d1852d94fbf402f08dc3725aac550b7618
PiKesi <73263112+PiKesi522@users.noreply.github.com>
2022-05-04 00:10:53 +0800

    Create P1823.java

---
+++ P1823.java
@@ -0,0 +1,9 @@
+public class P1823 {
+    public int findTheWinner(int n, int k) {
+        int p = 0;
+        for (int i = 2; i <= n; i++) {
+            p = (p + k) % i;
+        }
+        return p + 1;
+    }
+}
